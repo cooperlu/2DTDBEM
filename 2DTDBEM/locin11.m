737f31ba31a98f49d36041fe23d566cb0d613ae9
cooperlu <lut@rose-hulman.edu>
2018-01-08 15:47:27 -0500

    Add files via upload

--- 2DTDBEM/locin11.m
+++ 2DTDBEM/locin11.m
@@ -1,5 +1,6 @@
-function [GW,HW] = locin11(sr,eta1,eta2,istep,at,ro,cp,cs)
-HW = zeros(2);
+function [HW,GW] = locin11(sr,eta1,eta2,istep,at,ro,cp,cs)
+GW = zeros(2);
+HW = GW;
 time0 = istep*at;
 time1 = (istep-1)*at;
 
@@ -9,6 +10,24 @@ s0 = max([1,cs*time0/sr]);
 s1 = max([1,cs*time1/sr]);
 raiz0 = raiz(p0) + p0/s0*raiz(s0);
 raiz1 = raiz(p1) + p1/s1*raiz(s1);
+act = (time0 * (log(p0+raiz(p0))/p0 + sin(1/p0))-...
+    time0 *(log(p1+raiz(p1))/p1 + sin(1/p1)))/cp+...
+    (time0 * (log(s0+raiz(s0))/s0 + sin(1/s0))-...
+    time0 *(log(s1+raiz(s1))/s1 + sin(1/s1)))/cs;
+bct = (time0*sin(1/p0)-time1*sin(1/p1))/cp-...
+    (time0*asin(1/s0)-time1*sin(1/s1))/cs;
+if p0>1
+    bct = bct-time0/cp*(1-(p0/s0)^2)/raiz0;
+end
+if p1>1
+    bct = bct+time1/cp*(1-(p1/s1)^2)/raiz1;
+end
+glocal11 = (act-bct)/2/pi/ro;
+glocal22 = (act+bct)/2/pi/ro;
+GW(1,1) = glocal11*eta2^2+glocal22*eta1^2;
+GW(1,2) = (glocal22-glocal11)*eta1*eta2;
+GW(2,1) = GW(1,2);
+GW(2,2) = glocal11*eta1^2+glocal22*eta2^2;
 
 
 end
--- 2DTDBEM/main.m
+++ 2DTDBEM/main.m
@@ -1,40 +1,103 @@
-%%
+clc
+clear all
+%% INPUT
+% Parameters
+E = 2; % Young's modulus
+ro = 2; % density
+nu = 0; % Possion's Ratio
+GE = E/(2*(1+nu)); % Shear Modulus
+cs = sqrt(GE/ro);
+cp = cs*sqrt((2-2*nu)/(1-2*nu));
+at = 0.05; % Time Step Size
+nstep = 150; % Total Time Step
 
-n = 32;
+% Geometry
+% Original Mesh
+x = [0:0.1:1,ones([1,9]),1:-0.1:0,zeros([1,9])];
+y = [zeros([1,10]),0:0.1:1,ones([1,9]),1:-0.1:0.1];
+n = 40;
+nn = 2*n;
 
-%%
+% Initialize
+bcun = zeros([2*n,nstep]); % matrix store solution to Ax = B
+bck = zeros([2*n,nstep]); % known boundary condition; NEED MOD
+bck(21:2:39,10:end) = ones(size(bck(21:2:39,10:end))); % HARD CODING!!!!!
+kode =  zeros([2*n,nstep]); % type of BC
+kode(61:80,:) = ones(size(kode(61:80,:)));% HARD CODING!!!!!
+udata = bcun; % Matrix Store u data; structure: [sptial x temporal];
+pdata = bcun; % Matrix Store p data; structure: [sptial x temporal];
+Hdata = zeros([2*n,2*n,nstep]); % Matrix store H^nm; 3rd index = n-m+1; struc: [nn x nn x temporal]
+Gdata = zeros([2*n,2*n,nstep]); % Matrix store G^nm; 3rd index = n-m+1; struc: [nn x nn x temporal]
 
-x(n+1) = x(1);
-y(n+1) = y(1);
-xm = (x(1:end-1)+x(2:end))./2;
-ym = (y(1:end-1)+y(2:end))./2;
-sl = sqrt((xm-x(1:n)).^2 + (ym-y(1:n)).^2);
-etax = (ym - y(1:n))./sl;
-etay = (x(1:n) - xm)./sl;
-
-%%
-
-for i = 1:n
-    for j = 1:n
-        if i == j;
-            locin11(
-            if istep  == 1
-                HT(1,1) = 0.5;
-                HT(2,2) = 0.5;
-            end
-            G(2*i-1:2*i,2*j-1:2*j) = GT(1:2,1:2);
-            H(2*i-1:2*i,2*j-1:2*j) = HT(1:2,1:2);
-        else
-            dist = distc
-            if dist > cp*istip*at
+%% CALCULATION
+for istep = 1:nstep
+    fprintf('Step %5d',istep);
+    
+    
+    if istep == 1
+        x(n+1) = x(1);
+        y(n+1) = y(1);
+        xm = (x(1:end-1)+x(2:end))./2;
+        ym = (y(1:end-1)+y(2:end))./2;
+        sl = sqrt((xm-x(1:n)).^2 + (ym-y(1:n)).^2);
+        etax = (ym - y(1:n))./sl;
+        etay = (x(1:n) - xm)./sl;
+    end
+    %%
+    
+    for i = 1:n
+        for j = 1:n
+            if i == j
+                [Hws,Gws] = locin11(sl(j),etax(j),etay(j),istep,at,ro,cp,cs);
+                if istep  == 1
+                    Hws(1,1) = 0.5;
+                    Hws(2,2) = 0.5;
+                end
+                G(2*i-1:2*i,2*j-1:2*j) = Gws(1:2,1:2);
+                H(2*i-1:2*i,2*j-1:2*j) = Hws(1:2,1:2);
             else
-                extin11
-                G(2*i-1:2*i,2*j-1:2*j) = GT(1:2,1:2);
-                H(2*i-1:2*i,2*j-1:2*j) = HT(1:2,1:2);
+                dist(i,j) = distc(xm(i),ym(i),x(j),y(j),xm(j),ym(j),sl(j));
+                
+                [Hws,Gws] = extin11(xm(i),ym(i),x(j),y(j),x(j+1),y(j+1),...
+                    sl(j),etax(j),etay(j),at,istep,cp,cs,nu,GE,ro);
+                G(2*i-1:2*i,2*j-1:2*j) = Gws(1:2,1:2);
+                H(2*i-1:2*i,2*j-1:2*j) = Hws(1:2,1:2);
             end
         end
+    end
+    
+    
+    Gdata(1:2*n,1:2*n,istep) = G;
+    Hdata(1:2*n,1:2*n,istep) = H;
+    if istep == 1
+        % A and B is Hnn and Gnn, does not change
         
-        
-        
+        A = H;
+        B = G;
+        for j = 1:nn
+            if kode(j,istep) == 1
+                A(:,j) = -G(:,j);
+                B(:,j) = -H(:,j);
+            end
+        end
+        FP = zeros([2*n,1]);
+    else
+        % Historical Effect
+        FP = zeros([2*n,1]);
+        for step = 2:istep
+            FP = FP + Gdata(1:2*n,1:2*n,step)*pdata(1:2*n,istep+1-step)-...
+                Hdata(1:2*n,1:2*n,step)*udata(1:2*n,istep+1-step);
+        end
+    end
+    
+    bcun(1:2*n,istep) = A\(B*bck(1:2*n,istep)+FP);
+    % Reorder and save u and p data
+    udata(1:2*n,istep) = bcun(1:2*n,istep);
+    pdata(1:2*n,istep) = bck(1:2*n,istep);
+    for j = 1:nn
+        if kode(j,istep) == 1
+            udata(j,istep) = bck(j,istep);
+            pdata(j,istep) = bcun(j,istep);
+        end
     end
 end
